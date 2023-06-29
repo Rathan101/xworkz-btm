57923d1a770f0279bacaed92aac01ae3a8ef68b0
Rathan101 <rathangowda071@gmail.com>
2023-06-29 11:10:41 +0530

    java

---
+++ Bike.java
@@ -0,0 +1,7 @@
+class Bike
+{
+	Bike()
+	{
+		System.out.println("Running Bike");
+	}
+}
---
+++ BikeRunner.java
@@ -0,0 +1,17 @@
+class BikeRunner
+{
+	public static void main("Running main class")
+	{
+		Bike bike=new Bike();
+		Bike name=new Bike();
+		Bike bikeno=new Bike();
+		Bike manufacture=new Bike();
+		Bike silensor=new Bike();
+		Bike cc=new Bike();
+		Bike mileage=new Bike();
+		Bike fueltank=new Bike();
+		Bike fuelconsumption=new Bike();
+		Bike topspeed=new Bike();
+		Bike color=new Bike();
+	}
+}
