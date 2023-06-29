048b64435307da0321f98b75c6363339c522071f
Rathan101 <rathangowda071@gmail.com>
2023-06-29 21:52:21 +0530

    java

---
+++ car/TurnLights.java
@@ -0,0 +1,8 @@
+class TurnLights{
+	static void mode(){
+		System.out.println("Turn Indicator mode");
+	}
+	static void mode(String parking){
+		System.out.println("Parking mode");
+	}
+}
---
+++ car/Wheel.java
@@ -0,0 +1,8 @@
+class Wheel{
+	static void airPresure(){
+		System.out.println("Air presure OK on all four wheels");
+	}
+	static void airPresure(String frontRight){
+		System.out.println("Air presure low on Front-Right tyer!!");
+	}
+}
